215bcafe421d4ede4003b3c379b46a3f52907f47
MagSG-7274 <49731971+MagSG-7274@users.noreply.github.com>
2021-02-16 13:03:56 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2, os
+
+# Capturing video
+name = input("Your Name? \n")
+num_of_photos: int = input("How many photos do you want to take?\n")
+
+webcam = cv2.VideoCapture(0)
+os.chdir("out")
+
+for x in range(1, int(num_of_photos) + 1):
+
+    file_name = name + str(x) + ".jpg"
+    ret, frame = webcam.read()
+    print("Took a photo number " + str(x) +": " + str(ret))
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite(file_name, gray)
+
+webcam.release()
